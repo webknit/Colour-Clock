20e27180618180f116a2256a7b4a538636b08958
Shane Prendergast <contact@webknit.co.uk>
2015-02-03 17:31:49 +0000

    reworking how the status is stored

--- script.js
+++ script.js
@@ -16,7 +16,7 @@ ColorClock.getTheTime = function() {
 	document.getElementById('js-time').innerHTML = this.hours + ':' + this.mins + ':' + this.secs ;
 
 	this.changeBgColour(this.hours, this.mins, this.secs);
-	this.checkText(this.hours, this.day);
+	this.checkText();
 
 }
 
@@ -26,53 +26,104 @@ ColorClock.changeBgColour = function(hour, min, sec) {
 
 }
 
-ColorClock.checkText = function(hour, day) {
+ColorClock.checkText = function() {
+
+	// http://codepen.io/anon/pen/PwJROZ
+
+	var statuses = {
+		weekend: 'What are you doing here? It\'s the weekend',
+		dinnerTime: 'It\'s dinner time!!',
+		homeTime: 'It\'s hometime, get yourself home!',
+		crazy: 'You are one crazy worker!',
+		morning: 'Morning everyone!',
+		morningGoodWeekend: 'Morning everyone! Hope you\'ve had a good weekend!'
+	};
+
+	var cases = [
+		{value: 'day', equals: [0, 6], status: statuses.weekend},
+		{value: 'hour', lowerLimit: 12, upperLimit: 13, status: statuses.dinnerTime},
+		{value: 'hour', lowerLimit: 17, upperLimit: 23, status: statuses.homeTime},
+		{value: 'hour', lowerLimit: 16, upperLimit: 18, status: statuses.homeTime}
+	];
 
 	var status;
+	var hour = this.hour;
+	var day = this.day;
 
-	console.log(day);
+	for (var i = 0; i > cases.length; i++) {
 
-	if(day == 6 || day == 0) {
+		var currentCase = cases[i];
+		var value = values[currentCase.value];
 
-		status = "What are you doing here? It's the weekend!"
+		if(typeof currentCase.equals !== 'undefined') {
 
-	}
+			if(currentCase.equals.indexOf(value)) { 
 
-	else if(hour >= 12 && hour < 13) {
+				status = currentCase.status;
+				console.log(currentCase);
 
-		status = "It's dinner time!!"
+			}
 
-	}
+		}
 
-	else if(hour >= 17 && hour < 23) {
+		else if(value >= currentCase.lowerLimit && value < currentCase.upperLimit) {
 
-		status = "It's hometime, get yourself home!"
+			status = currentCase.status;
+			console.log(currentCase);
 
-	}
+		}
 
-	else if(hour >= 23 && hour < 07) {
+		else {
 
-		status = "You are one crazy worker!"
+			status = '';
 
-	}
+		}
 
-	else if(hour >= 07 && hour < 19 && day != 6 && day != 0) {
 
-		status = "Morning everyone!"
-
-	}
-
-	else if(hour >= 07 && hour < 19 && day == 1) {
-
-		status = "Morning everyone! Hope you've had a good weekend!"
 
 	}
 
-	else {
+	// if(day == 6 || day == 0) {
 
-		status = ""
+	// 	status = statuses.weekend;
 
-	}
+	// }
+
+	// else if(hour >= 12 && hour < 13) {
+
+	// 	status = statuses.dinnerTime;
+
+	// }
+
+	// else if(hour >= 17 && hour < 23) {
+
+	// 	status = statuses.homeTime;
+
+	// }
+
+	// else if(hour >= 23 && hour < 07) {
+
+	// 	status = statuses.crazy;
+
+	// }
+
+	// else if(hour >= 07 && hour < 19 && day != 6 && day != 0) {
+
+	// 	status = statuses.morning;
+
+	// }
+
+	// else if(hour >= 07 && hour < 19 && day == 1) {
+
+	// 	status = statuses.morningGoodWeekend;
+
+	// }
+
+	// else {
+
+	// 	status = '';
+
+	// }
 
 	document.getElementById('js-announcement').innerHTML = status
 
