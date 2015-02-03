dd6e36d668c0137d95fbf319869a082fbfaa99fe
Shane Prendergast <contact@webknit.co.uk>
2015-02-03 16:19:50 +0000

    adding message functionality

--- script.js
+++ script.js
@@ -2,7 +2,8 @@ var ColorClock = ColorClock || {};
 
 ColorClock.getTheTime = function() {
 
-	this.today = new Date(); 
+	this.today = new Date();
+	this.day = ColorClock.today.getDay();
 	this.hours = ColorClock.today.getHours();
 	this.mins = ColorClock.today.getMinutes(); 
 	this.secs = ColorClock.today.getSeconds();
@@ -11,16 +12,69 @@ ColorClock.getTheTime = function() {
 	if(this.mins < 10) this.mins = '0' + this.mins;
 	if(this.secs < 10) this.secs = '0' + this.secs;
 
-	console.log('time is ' + this.hours + ':' + this.mins + ':' + this.secs);
-	document.getElementById('time').innerHTML = this.hours + ':' + this.mins + ':' + this.secs ;
+	//console.log('date is ' + this.day + ' time is ' + this.hours + ':' + this.mins + ':' + this.secs);
+	document.getElementById('js-time').innerHTML = this.hours + ':' + this.mins + ':' + this.secs ;
 
-	this.changeBgColour(this.hours, this.mins, this.secs )
+	this.changeBgColour(this.hours, this.mins, this.secs);
+	this.checkText(this.hours, this.day);
 
 }
 
 ColorClock.changeBgColour = function(hour, min, sec) {
 
 	document.body.style.backgroundColor = '#' + hour + min + sec ;
+
+}
+
+ColorClock.checkText = function(hour, day) {
+
+	var status;
+
+	console.log(day);
+
+	if(day == 6 || day == 0) {
+
+		status = "What are you doing here? It's the weekend!"
+
+	}
+
+	else if(hour >= 12 && hour < 13) {
+
+		status = "It's dinner time!!"
+
+	}
+
+	else if(hour >= 17 && hour < 23) {
+
+		status = "It's hometime, get yourself home!"
+
+	}
+
+	else if(hour >= 23 && hour < 07) {
+
+		status = "You are one crazy worker!"
+
+	}
+
+	else if(hour >= 07 && hour < 19 && day != 6 && day != 0) {
+
+		status = "Morning everyone!"
+
+	}
+
+	else if(hour >= 07 && hour < 19 && day == 1) {
+
+		status = "Morning everyone! Hope you've had a good weekend!"
+
+	}
+
+	else {
+
+		status = ""
+
+	}
+
+	document.getElementById('js-announcement').innerHTML = status
 
 }
 
